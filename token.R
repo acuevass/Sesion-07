997e19e6d67f4d8dc51de7441a83b0acb8bccdad
Alfredo Cuevas <acuevass@yahoo.com.mx>
2022-08-10 02:05:46 +0000

    Prueba

---
+++ token.R
@@ -0,0 +1,8 @@
+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
+
+install.packages("gitcreds")
+library(gitcreds)
+
+gitcreds_set() # Para ingresar el token
+
+gitcreds_get() # Sirve para verificar que se ingresó el token
